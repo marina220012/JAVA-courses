a98f1c6abf5c5845953e93a680157983df431f76
Marina Tikhinia <88191244+marina220012@users.noreply.github.com>
2021-12-28 18:53:47 +0300

    Update CalculatorWithCounterClassicMain.java

--- home_work_3/runners/CalculatorWithCounterClassicMain.java
+++ home_work_3/runners/CalculatorWithCounterClassicMain.java
@@ -7,7 +7,7 @@ public class CalculatorWithCounterClassicMain {
         CalculatorWithCounterClassic number=new CalculatorWithCounterClassic();
         int resultInt=number.multiplication(15,7);
         number.incrementCountOperation();
-        double resultDouble2 = number.division(28,5);
+        double resultDouble2 = number.division(28.0,5);
         number.incrementCountOperation();
         resultDouble2= number.pow(resultDouble2, 2);
         number.incrementCountOperation();
@@ -16,7 +16,7 @@ public class CalculatorWithCounterClassicMain {
         resultDouble1= number.add(resultDouble1, resultDouble2);
         //number.add(number.add(4.1, number.multiplication(15,7)),
         //                number.pow(number.division(28,5), 2))
-        // 4.1 + 15 * 7 + (28 / 5) ^ 2=134.1
+        // 4.1 + 15 * 7 + (28 / 5) ^ 2=140.4599999
         number.incrementCountOperation();
         System.out.println("Результат "+ resultDouble1);
         System.out.println("Количество операций "+number.getCountOperation());
