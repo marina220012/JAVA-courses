bdacc3524b555ec59852f297cbb99cc99ea9454b
Marina Tikhinia <88191244+marina220012@users.noreply.github.com>
2021-12-28 18:52:29 +0300

    Update CalculatorWithOperatorMain.java

--- home_work_3/runners/CalculatorWithOperatorMain.java
+++ home_work_3/runners/CalculatorWithOperatorMain.java
@@ -6,7 +6,6 @@ public class CalculatorWithOperatorMain {
     public static void main(String[] args) {
         CalculatorWithOperator number=new CalculatorWithOperator();
         double res= number.add(number.add(4.1, number.multiplication(15,7)),
-                number.pow(number.division(28,5), 2));//134.1
-        System.out.println("Результат "+res);
+                number.pow(number.division(28.0,5), 2));//140.4599999
     }
 }
